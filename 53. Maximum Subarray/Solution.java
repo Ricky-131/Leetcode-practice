e954a630656e3cc76bd75ba0110aedb286e5d14d
Ricky S <24cs131@stjosephs.ac.in>
2025-08-08 17:55:18 +0530

    Create Solution.java

---
+++ 53. Maximum Subarray/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int msum = nums[0];
+        int csum = msum;
+        for(int i=1 ; i<nums.length ; i++){
+            csum = Math.max(nums[i],csum+nums[i]);
+            msum = (msum<csum)?csum:msum;
+        }
+        return msum;
+    }
+}
