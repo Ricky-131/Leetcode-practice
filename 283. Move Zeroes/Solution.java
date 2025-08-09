18720640b9d2af3fd7ffff4e75456c7068ef1082
Ricky S <24cs131@stjosephs.ac.in>
2025-08-09 18:02:05 +0530

    Create Solution.java

---
+++ 283. Move Zeroes/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+        for(int ele : nums){
+            if(ele!=0){
+                nums[index++] = ele;
+            }
+        }
+        while(index<nums.length){
+            nums[index++] = 0;
+        }
+    }
+}
