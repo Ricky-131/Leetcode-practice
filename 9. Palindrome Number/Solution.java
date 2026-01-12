48a8df73bc55977cfe8c5eab1d71ece421ff19a5
Ricky S <24cs131@stjosephs.ac.in>
2026-01-12 21:42:16 +0530

    Create Solution.java

---
+++ 9. Palindrome Number/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int temp = x;
+        int num = 0;
+        for(int i=temp%10 ; temp>0 ; temp/=10 , i=temp%10){
+            num = (num*10)+i;
+        }
+        return (x == num);
+    }
+}
