2032d35bdb7664efc66492788f9a57bd52d67752
Ricky S <24cs131@stjosephs.ac.in>
2026-01-12 21:44:13 +0530

    Create Solution.java

---
+++ 26. Remove Duplicates from Sorted Array/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        Set<Integer> l = new HashSet<>();
+        int ind = 0;
+        for(int ele : nums){
+            if(!l.contains(ele)){
+                nums[ind++] = ele;
+                l.add(ele); 
+            }
+        }
+        return ind;
+    }
+}
