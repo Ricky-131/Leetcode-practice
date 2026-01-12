bc2118d9e09fa000efe1e91b895fab47276b5c3f
Ricky S <24cs131@stjosephs.ac.in>
2026-01-12 21:54:01 +0530

    Create Solution.java

---
+++ 231. Power of Two/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        double i;
+        if(n<0){
+            return false;
+        }
+        for(i=n ; i>1 ; i/=2.0){
+
+        }
+        return i==1.0;
+    }
+}
