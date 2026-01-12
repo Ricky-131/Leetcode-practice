1fe2b6a810419858230d95fd19ef5c7ea2cdaaf8
Ricky S <24cs131@stjosephs.ac.in>
2026-01-12 22:07:08 +0530

    Create Solution.java

---
+++ 961. N-Repeated Element in Size 2N Array/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        int n = nums.length / 2;
+        Set<Integer> newnums = new HashSet<>();
+        for(int i=0 ; i<n*2 ; i++){
+            if(newnums.contains(nums[i])){
+                return nums[i];
+            }
+            newnums.add(nums[i]);
+        }
+        return -1;
+    }
+}
