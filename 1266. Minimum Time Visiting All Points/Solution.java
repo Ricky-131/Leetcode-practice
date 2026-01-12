adb59da200956ecea61cfec6b0ada79173b595d0
Ricky S <24cs131@stjosephs.ac.in>
2026-01-12 21:39:21 +0530

    Create Solution.java

---
+++ 1266. Minimum Time Visiting All Points/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int time = 0;
+        int n = points.length;
+        for(int i=0 ; i<n-1 ; i++){
+            int[] coord1 = points[i];
+            int[] coord2 = points[i+1];
+            int x1 = coord1[0];
+            int y1 = coord1[1];
+            int x2 = coord2[0];
+            int y2 = coord2[1];
+            int xdiff = Math.abs(x1-x2);
+            int ydiff = Math.abs(y1-y2);
+            int t = (xdiff>ydiff)?xdiff:ydiff;
+            time+=t;
+        }
+        return time;
+    }
+}
