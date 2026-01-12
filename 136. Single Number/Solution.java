fd1749821c0077d43b0407db7b308a3393a0902b
Ricky S <24cs131@stjosephs.ac.in>
2026-01-12 21:49:01 +0530

    Create Solution.java

---
+++ 136. Single Number/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Iterator;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0 ; i<nums.length ; i++){
+            if(set.contains(nums[i]))
+            {
+                set.remove(nums[i]);
+            }
+            else{
+                set.add(nums[i]);
+            }
+        }
+        return set.iterator().next() ;
+    }
+}
