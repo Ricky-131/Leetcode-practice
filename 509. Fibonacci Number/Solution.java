415e6e08070cd3d6b27ae35878c3d698144d5ed3
Ricky S <24cs131@stjosephs.ac.in>
2026-01-12 22:02:08 +0530

    Create Solution.java

---
+++ 509. Fibonacci Number/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int fib(int n) {
+        if(n<2){
+            return n;
+        }
+        int num=1;
+        int a=0 , b=1;
+        for(int i=2 ; i<=n ; i++){
+            num = a+b;
+            a=b;
+            b=num;
+        }
+        return num;
+    }
+}
