6e64763416b04937279ecc91d4930c56ee9e20ef
lew2612 <143020761+lew2612@users.noreply.github.com>
2026-02-08 22:36:50 +0530

    solved on 08.02.2026

---
+++ LC110_Balanced_Binary_Tree/TreeNode.java
@@ -0,0 +1,30 @@
+package LC110_Balanced_Binary_Tree;
+import java.util.*;
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return dfs(root)!=-1;
+    }
+
+    int dfs(TreeNode node) {
+        if (node == null) return 0;
+        int l = dfs(node.left);
+        if (l == -1) return -1;
+        int r = dfs(node.right);
+        if (r == -1) return -1;
+        if (Math.abs(l - r) > 1) return -1;
+        return Math.max(l, r) + 1;
+    }
+}
