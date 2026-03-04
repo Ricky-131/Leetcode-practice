a7fa7b2fd4a8e5bb29a5634388bd48dd82c02bc1
Ricky-131 <24cs131@stjosephs.ac.in>
2026-03-04 23:04:16 +0530

    solved on 04.03.2026

---
+++ LC1582_Special_Positions_in_a_Binary_Matrix/Solution.java
@@ -0,0 +1,22 @@
+package LC1582_Special_Positions_in_a_Binary_Matrix;
+import java.util.*;
+class Solution {
+    public int numSpecial(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int res = 0;
+        int[] row = new int[m];
+        int[] col = new int[n];
+        for (int i=0; i<m; i++)
+            for (int j=0; j<n; j++)
+                if (mat[i][j]==1){
+                    row[i]++; 
+                    col[j]++;
+                }
+        for (int i=0; i<m; i++)
+            for (int j=0; j<n; j++)
+                if (mat[i][j]==1 && row[i]==1 && col[j]==1) 
+                    res++;
+        return res;
+    }
+}
