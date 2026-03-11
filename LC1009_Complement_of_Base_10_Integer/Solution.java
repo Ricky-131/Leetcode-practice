302cac87b58fde8bf633e66a9820c23e4553e98b
Ricky-131 <24cs131@stjosephs.ac.in>
2026-03-11 21:21:48 +0530

    solved on 11.03.2026

--- LC1009_Complement_of_Base_10_Integer/Solution.java
+++ LC1009_Complement_of_Base_10_Integer/Solution.java
@@ -2,6 +2,6 @@ package LC1009_Complement_of_Base_10_Integer;
 import java.util.*;
 class Solution {
     public int bitwiseComplement(int n) {
-        return n^(int)(Math.pow(2,Integer.toBinaryString(n).length())-1);
+        return n^(int)(Math.pow(2, Integer.toBinaryString(n).length()) - 1);
     }
 }
