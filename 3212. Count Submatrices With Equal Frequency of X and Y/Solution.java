16cb260bfc2b00ec1975571be8c08d28cf152f3d
Ricky-131 <24cs131@stjosephs.ac.in>
2026-03-19 19:53:41 +0530

    solved on 19.03.2026

---
+++ 3212. Count Submatrices With Equal Frequency of X and Y/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numberOfSubmatrices(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int count = 0;
+        int[][] xcount = new int[m][n];
+        int[][] prefmat = new int[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 'X'){
+                    prefmat[i][j] = 1;
+                    xcount[i][j] = 1;
+                }
+                if(grid[i][j] == 'Y')   prefmat[i][j] = -1;
+                if(grid[i][j] == '.')   prefmat[i][j] = 0;
+                
+                if(i>0) prefmat[i][j] += prefmat[i-1][j];
+                if(j>0) prefmat[i][j] += prefmat[i][j-1];
+                if(i>0 && j>0)  prefmat[i][j] -= prefmat[i-1][j-1];
+                
+                if(i>0) xcount[i][j] += xcount[i-1][j];
+                if(j>0) xcount[i][j] += xcount[i][j-1];
+                if(i>0 && j>0)  xcount[i][j] -= xcount[i-1][j-1];
+                
+                if((prefmat[i][j] == 0) && xcount[i][j]>0)  count++;
+            }
+        }
+        return count;
+    }
+}
