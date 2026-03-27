9ecd82aa863019d780b810492fddda4d4781a173
Ricky-131 <24cs131@stjosephs.ac.in>
2026-03-27 22:03:27 +0530

    solved on 27.03.2026

---
+++ LC2946_Matrix_Similarity_After_Cyclic_Shifts/Solution.java
@@ -0,0 +1,20 @@
+package LC2946_Matrix_Similarity_After_Cyclic_Shifts;
+import java.util.*;
+class Solution {
+    public boolean areSimilar(int[][] mat, int k) {
+        int m = mat.length;
+        int n = mat[0].length;
+        k %= n;
+        for(int i=0 ; i<m ; i++){
+            for(int j=0 ; j<n ; j++){
+                if(i%2==0){
+                    if(mat[i][j] != mat[i][(j+k)%n])    return false;
+                }
+                else{
+                    if(mat[i][j] != mat[i][(j-k+n)%n])  return false;
+                }
+            }
+        }
+        return true;
+    }
+}
