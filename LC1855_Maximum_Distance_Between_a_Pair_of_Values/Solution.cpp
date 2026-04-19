ac735c51e1e920aab6814efeb2574cb5bf0fa67a
Ricky-131 <24cs131@stjosephs.ac.in>
2026-04-19 23:28:18 +0530

    solved on 19.04.2026

---
+++ LC1855_Maximum_Distance_Between_a_Pair_of_Values/Solution.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+        int i = 0;
+        int j = 0;
+        int res = 0;
+        int n = nums1.size();
+        int m = nums2.size();
+        while(i < n && j < m){
+            if(nums1[i] <= nums2[j]){
+                res = max(res, j - i);
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+        return res;
+    }
+};
