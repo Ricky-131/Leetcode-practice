80ab6d896950c660ad9a3e2774d2482b9a1d958d
Ricky-131 <24cs131@stjosephs.ac.in>
2026-05-13 20:16:30 +0530

    solved on 13.05.2026

--- LC1665_Minimum_Initial_Energy_to_Finish_Tasks/Solution.java
+++ LC1665_Minimum_Initial_Energy_to_Finish_Tasks/Solution.java
@@ -1,2 +1,12 @@
 package LC1665_Minimum_Initial_Energy_to_Finish_Tasks;
 import java.util.*;
+class Solution {
+    public int minimumEffort(int[][] tasks) {
+        Arrays.sort(tasks, (a, b) -> (a[1] - a[0]) - (b[1] - b[0]));
+        int res = 0;
+        for(int i=0; i<tasks.length; i++) {
+            res = Math.max(res + tasks[i][0], tasks[i][1]);
+        }
+        return res;
+    }
+}
