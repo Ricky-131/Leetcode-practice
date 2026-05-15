5e2d7e7116736b85d962e2e8871029771d718a88
Ricky S <24cs131@stjosephs.ac.in>
2026-05-15 23:23:35 +0530

    solved on 15.05.2026

--- LC2784_Check_if_Array_is_Good/Solution.java
+++ LC2784_Check_if_Array_is_Good/Solution.java
@@ -1,6 +1,20 @@
 package LC2784_Check_if_Array_is_Good;
 import java.util.*;
 
-public class Solution {
-    
+class Solution {
+    public boolean isGood(int[] nums) {
+        int n = nums.length - 1;
+        boolean dupl = false;
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums) {
+            if(num > n) return false;
+            if(set.contains(num)){
+                if(num < n || dupl) return false;
+                dupl = true;
+                continue;
+            }
+            set.add(num);
+        }
+        return true;
+    }
 }
