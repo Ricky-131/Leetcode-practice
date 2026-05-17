47e6c03143e67f2bc19305e47ff88fca3e0596f8
Ricky S <24cs131@stjosephs.ac.in>
2026-05-17 23:31:44 +0530

    solved on 17.05.2026

---
+++ LC1306_Jump_Game_III/Solution.java
@@ -0,0 +1,19 @@
+package LC1306_Jump_Game_III;
+import java.util.*;
+public class Solution {
+    public boolean canReach(int[] arr, int start) {
+        int n = arr.length;
+        boolean[] vis = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        while(!q.isEmpty()){
+            int i = q.poll();
+            if(i < 0 || i >= n || vis[i])   continue;
+            if(arr[i] == 0) return true;
+            vis[i] = true;
+            q.offer(i - arr[i]);
+            q.offer(i + arr[i]);
+        }
+        return false;
+    }
+}
